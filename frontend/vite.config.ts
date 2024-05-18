8f0517c4dba7fb07cd77d6a8be179312ad013937
maciekzygmunt <maciekzygmunt@interia.pl>
2024-05-18 13:34:30 +0200

    install noir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		copy({
+			targets: [
+				{ src: "node_modules/**/*.wasm", dest: "node_modules/.vite/dist" },
+			],
+			copySync: true,
+			hook: "buildStart",
+		}),
+	],
+	esbuild: {
+		target: "esnext",
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "esnext",
+		},
+	},
+	server: {
+		port: 2137,
+	},
+});
