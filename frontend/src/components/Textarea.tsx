a3ee3c4eb742b54ef36557926f3ea3d0a7dd4a3e
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-05-18 23:34:30 +0200

    prove with private input

--- frontend/src/components/Textarea.tsx
+++ frontend/src/components/Textarea.tsx
@@ -2,6 +2,7 @@ import type { ClassNameValue } from "tailwind-merge";
 import cn from "../utils/cn";
 
 type TextareaProps = {
+	label: string;
 	value: string;
 	setValue: (value: string) => void;
 	className?: ClassNameValue;
@@ -9,20 +10,48 @@ type TextareaProps = {
 };
 
 export default function Textarea({
+	label,
 	className,
 	placeholder,
 	value,
 	setValue,
 }: TextareaProps) {
 	return (
-		<textarea
-			placeholder={placeholder}
-			value={value}
-			onChange={(e) => setValue(e.currentTarget.value)}
-			className={cn(
-				"bg-gray-200 rounded border p-1 font-medium border-gray-300 w-full min-h-20",
-				className,
-			)}
-		/>
+		<label className="flex flex-col gap-1">
+			<span className="font-medium">{label}</span>
+
+			<textarea
+				placeholder={placeholder}
+				value={value}
+				onChange={(e) => setValue(e.currentTarget.value)}
+				className={cn(
+					"bg-gray-200 rounded border p-1 font-medium border-gray-300 w-full min-h-20",
+					className,
+				)}
+			/>
+		</label>
+	);
+}
+
+export function Input({
+	label,
+	className,
+	placeholder,
+	value,
+	setValue,
+}: TextareaProps) {
+	return (
+		<label className="flex flex-col gap-1">
+			<span className="font-medium">{label}</span>
+			<input
+				placeholder={placeholder}
+				value={value}
+				onChange={(e) => setValue(e.currentTarget.value)}
+				className={cn(
+					"bg-gray-200 rounded border p-1 font-medium border-gray-300 w-full",
+					className,
+				)}
+			/>
+		</label>
 	);
 }
--- frontend/src/pages/verify.tsx
+++ frontend/src/pages/verify.tsx
@@ -1,25 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
-import Textarea from "../components/Textarea";
+import Textarea, { Input } from "../components/Textarea";
+import { Noir } from "@noir-lang/noir_js";
+import {
+	BarretenbergBackend,
+	CompiledCircuit,
+} from "@noir-lang/backend_barretenberg";
+import circuit from "../../../target/mnist.json";
 
 function Verify() {
-	const [proof, setProof] = useState<string>("");
+	const [proof, setProof] = useState("");
+	const [noir, setNoir] = useState<Noir>();
+	const [digit, setDigit] = useState("");
 
-	// https://noir-lang.org/docs/tutorials/noirjs_app
-	// const verifyProof = async () => {
-	// };
+	useEffect(() => {
+		// Load Noir
+		const backend = new BarretenbergBackend(circuit as CompiledCircuit);
+		const noir = new Noir(circuit as CompiledCircuit, backend);
+		setNoir(noir);
+	}, []);
+
+	async function handleOffChainVerification() {
+		if (!proof || !noir || !digit) return;
+
+		const proofAsByteArray = byteArrFromHexStr(proof);
+		console.log("Verifying...");
+		const result = await noir.verifyProof({
+			publicInputs: [digitToHexString(+digit)],
+			proof: proofAsByteArray,
+		});
+		console.log("Verification result: ", result);
+	}
 
 	return (
 		<div className="w-full flex flex-col gap-4 items-center justify-center">
 			<div className="w-[560px]">
 				<h1 className="text-2xl mb-2 font-semibold">Verify proof</h1>
-				<Textarea value={proof} setValue={setProof} />
+
+				<Input label="Digit" value={digit} setValue={setDigit} />
+				<Textarea label="Proof" value={proof} setValue={setProof} />
 				<div className="flex w-full justify-end mt-4">
-					<Button type="button">Verify proof</Button>
+					<Button type="button" onClick={() => handleOffChainVerification()}>
+						Verify proof
+					</Button>
 				</div>
 			</div>
 		</div>
 	);
+}
+function byteArrFromHexStr(hexString: string) {
+	const str = hexString.startsWith("0x") ? hexString.slice(2) : hexString;
+	if (str.length % 2 !== 0) throw new Error("Invalid hex string length");
+	const arr = [];
+	for (let i = 0; i < str.length; i += 2) {
+		arr.push(parseInt(str.substring(i, i + 2), 16));
+	}
+	return new Uint8Array(arr);
+}
+
+function digitToHexString(digit: number) {
+	return digit.toString(16).padStart(64, "0");
 }
 
 export default Verify;
