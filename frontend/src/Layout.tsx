a6a8ffe84ba3d490118aebba528d3a16e715f3bc
maciekzygmunt <maciekzygmunt@interia.pl>
2024-05-19 10:56:19 +0200

    restyle header

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="w-full h-screen sticky top-0">
-			<div className="h-20 shadow shadow-main flex text-main  justify-between w-screen items-center px-8 mb-8">
+			<div className="h-20 m-4 border border-main flex text-main rounded-lg justify-between items-center px-8 mb-8">
 				<div className="text-2xl font-bold uppercase">Kocioł</div>
-				<div className="flex gap-4 text-xl font-semibold">
-					<Link to="/">Draw</Link>
-					<Link to="/verify">Verify</Link>
+				<div className="flex gap-0 text-xl font-semibold">
+					<Link to="/" className="link">
+						Draw
+					</Link>
+					<Link to="/verify" className="link">
+						Verify
+					</Link>
 				</div>
 			</div>
 			<div className="px-4 h-full top-0">{children}</div>
